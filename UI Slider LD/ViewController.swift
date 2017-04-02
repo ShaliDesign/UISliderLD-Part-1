08c01f54216b56bc5e9ecc476e372e1936f987c4
Linh&ShawnD <linhshawn-d@LinhShawnDs-MacBook-Pro.local>
2017-04-02 20:40:40 +0700

    Initial Commit

---
+++ UI Slider LD/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UI Slider LD
+//
+//  Created by Linh&ShawnD on 2017-04-02.
+//  Copyright © 2017 Shali Design. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
